2ba50a7f7bb63c4307fb29de52bf76a2b1744d16
RarceD <akimbo170@gmail.com>
2023-05-30 18:48:27 +0200

    Fix performance issue with thread in BackgroundService

--- MeApuntoBackend/Services/PeriodicTaskService.cs
+++ MeApuntoBackend/Services/PeriodicTaskService.cs
@@ -7,6 +7,7 @@ public class PeriodicTaskService : BackgroundService
     private static ISchedulerRepository? _schedulerRepository;
     private static System.Timers.Timer? _bookerTimer;
     private static IServiceProvider? _serviceProvider;
+    private static ILogger<PeriodicTaskService>? _logger;
     private const int TIME_CLEAN_DB_SECONDS = 60 * 30; // Every 30 min I clear db
     public PeriodicTaskService(IServiceProvider serviceProvider)
     {
@@ -22,18 +23,19 @@ public class PeriodicTaskService : BackgroundService
     }
     private static void CleanBooks()
     {
-        Console.WriteLine("Cleaning books");
         if (_serviceProvider == null) return;
         using (IServiceScope scope = _serviceProvider.CreateScope())
         {
             var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            _logger = scope.ServiceProvider.GetRequiredService<ILogger<PeriodicTaskService>>();
+            _logger.LogWarning("[PERIODIC DELETE] at:" + DateTime.Now.ToString());
             _schedulerRepository = new SchedulerRepository(dbContext);
             ClearScheduleTable();
         }
     }
     private static void ClearScheduleTable()
     {
-        if ( _schedulerRepository == null) return;
+        if (_schedulerRepository == null) return;
         var allSchedules = _schedulerRepository.GetAll();
 
         var yestardayDay = DateTime.Now.AddDays(-1).ToShortDateString();
@@ -41,7 +43,11 @@ public class PeriodicTaskService : BackgroundService
         foreach (var scheduler in allSchedules)
         {
             if (scheduler.Day == yestardayDay)
+            {
                 _schedulerRepository.Remove(scheduler);
+                _logger.LogWarning("[PERIODIC DELETE] task: " + _schedulerRepository.ToPrint(scheduler));
+
+            }
         }
     }
 
@@ -49,8 +55,5 @@ public class PeriodicTaskService : BackgroundService
     {
         // Set timer to execute periodically:
         SetTimerBookerCleaner();
-
-        // Run forever:
-        while (!stoppingToken.IsCancellationRequested) await Task.Run(() => { });
     }
 }
