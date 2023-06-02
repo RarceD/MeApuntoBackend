dc3d0ec2bc5f906760765a5d5b8221a3ee4b95e5
RarceD <akimbo170@gmail.com>
2023-06-02 16:59:48 +0200

    Create db script

--- MeApuntoBackend/Scripts/CreateDb.sql
+++ MeApuntoBackend/Scripts/CreateDb.sql
@@ -1,3 +1,59 @@
+CREATE TABLE urbas (
+    id           INTEGER NOT NULL,
+    name         TEXT,
+    info         TEXT,
+    [key]        TEXT,
+    advance_book INTEGER,
+    PRIMARY KEY (
+        id
+    )
+);
+
+CREATE TABLE clients (
+    id       INTEGER,
+    urba_id  INTEGER,
+    name     TEXT,
+    username TEXT    UNIQUE,
+    pass     TEXT,
+    token    BLOB,
+    plays    INTEGER NOT NULL
+                     DEFAULT 0,
+    floor    TEXT,
+    letter   TEXT,
+    house    TEXT,
+    PRIMARY KEY (
+        id AUTOINCREMENT
+    )
+);
+
+CREATE TABLE Configuration (
+    Id        INTEGER PRIMARY KEY AUTOINCREMENT,
+    ValidHour TEXT,
+    CourtId   INTEGER
+);
+
+CREATE TABLE courts (
+    id          INTEGER,
+    name        TEXT,
+    urba_id     INTEGER,
+    type                NOT NULL
+                        DEFAULT 0,
+    valid_times TEXT,
+    PRIMARY KEY (
+        id AUTOINCREMENT
+    )
+);
+
+CREATE TABLE normative (
+    Id     INTEGER NOT NULL
+                   UNIQUE,
+    Title  TEXT,
+    Text   TEXT,
+    UrbaId INTEGER,
+    PRIMARY KEY (
+        Id AUTOINCREMENT
+    )
+);
 
 CREATE TABLE Scheduler (
     Id       INTEGER NOT NULL
@@ -5,13 +61,7 @@ CREATE TABLE Scheduler (
     CourtId  INTEGER,
     Time     TEXT,
     ClientId INTEGER,
-    Day      TEXT
-);
-
-
-CREATE TABLE Configuration (
-    Id        INTEGER PRIMARY KEY AUTOINCREMENT,
-    ValidHour TEXT,
-    CourtId   INTEGER
+    Day      TEXT,
+    Duration TEXT
 );
 
