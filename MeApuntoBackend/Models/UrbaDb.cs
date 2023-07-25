e51690c1f5cad4e2baa750ffd43290ea0e7319b3
RarceD <akimbo170@gmail.com>
2023-07-25 11:30:02 +0200

    Add new free param to Urbas, detect paid method

--- MeApuntoBackend/Models/UrbaDb.cs
+++ MeApuntoBackend/Models/UrbaDb.cs
@@ -1,9 +1,10 @@
 ﻿namespace MeApuntoBackend.Models;
-    public class UrbaDb
+public class UrbaDb
 {
     public int Id { get; set; }
     public string? name { get; set; }
     public string? info { get; set; }
     public string? key { get; set; }
     public int advance_book { get; set; }
+    public bool free { get; set; }
 }
