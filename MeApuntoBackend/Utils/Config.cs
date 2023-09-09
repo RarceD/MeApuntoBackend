a57429aab67bd421e96c4b1f93a4e152c2da85ff
RarceD <akimbo170@gmail.com>
2023-09-09 11:34:40 +0200

    Update config file

--- MeApuntoBackend/Utils/Config.cs
+++ MeApuntoBackend/Utils/Config.cs
@@ -3,6 +3,7 @@
     public const string EMAIL_SOURCE = "";
     public const string HOST = "";
     public const string PASS = "";
-    public const string PUBLIC_URL = "";
+    public const string PUBLIC_URL_PAID = "";
+    public const string PUBLIC_URL_FREE = "";
     public static Dictionary<string, int> URBA_CODES = new();
 }
