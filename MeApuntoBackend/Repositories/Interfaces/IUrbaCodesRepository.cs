870c22c19f9be3d31cf306c1f0f62b5bcef1e9df
RarceD <akimbo170@gmail.com>
2023-12-13 16:46:46 +0100

    Create urba codes respository

---
+++ MeApuntoBackend/Repositories/Interfaces/IUrbaCodesRepository.cs
@@ -0,0 +1,6 @@
+﻿using MeApuntoBackend.Models;
+
+namespace MeApuntoBackend.Repositories;
+public interface IUrbaCodesRepository : IRepository<UrbaCodesDb>
+{
+}
---
+++ MeApuntoBackend/Repositories/UrbaCodesRepository.cs
@@ -0,0 +1,38 @@
+﻿using MeApuntoBackend.Models;
+
+namespace MeApuntoBackend.Repositories;
+public class UrbaCodesRepository : IUrbaCodesRepository
+{
+    private readonly ApplicationDbContext _context;
+    public UrbaCodesRepository(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+    public void Add(UrbaCodesDb item)
+    {
+        _context.UrbaCodes.Add(item);
+        _context.SaveChanges();
+    }
+    public void Remove(UrbaCodesDb item)
+    {
+        _context.UrbaCodes.Remove(item);
+        _context.SaveChanges();
+    }
+    public void Update(UrbaCodesDb item)
+    {
+        _context.UrbaCodes.Update(item);
+        _context.SaveChanges();
+    }
+
+    public IEnumerable<UrbaCodesDb> GetAll()
+    {
+        List<UrbaCodesDb> urbas = _context.UrbaCodes.ToList();
+        return urbas;
+    }
+    public UrbaCodesDb GetById(int id)
+    {
+        UrbaCodesDb? urba = _context.UrbaCodes.Where(i => i.Id == id).FirstOrDefault();
+        if (urba == null) return new UrbaCodesDb();
+        return urba;
+    }
+}
