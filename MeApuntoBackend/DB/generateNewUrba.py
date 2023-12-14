a5582484fc0e3859a3d020957496aa185d67f3e2
RarceD <akimbo170@gmail.com>
2023-12-14 16:51:58 +0100

    Update generate urba script

--- MeApuntoBackend/DB/generateNewUrba.py
+++ MeApuntoBackend/DB/generateNewUrba.py
@@ -25,14 +25,31 @@ class UrbaClientGenerator():
         urbaFound = self.cur.fetchall()
         return urbaFound[0];
 
-    def create_court(self, court_name, urba_id):
-        sql_text = '''insert into courts (name, urba_id) values (?, ?);'''
-        self.cur.execute(sql_text)
-        conn.commit()
+    def create_court(self, name, urba_id, courtType='1', validTimes='1'):
+        sql_text = '''insert into courts (name, urba_id, type, valid_times) values (?,?,?,?);'''
+        self.cur.execute(sql_text, (name, urba_id, courtType, validTimes))
+        self.conn.commit()
+        return self.get_court_by_name(name)[0]
+
+    def get_court_by_name(self, court_name):
+        sql_text = '''select * from courts where name=?;'''
+        self.cur.execute(sql_text, (court_name, ))
+        self.conn.commit()
+        courtFound = self.cur.fetchall()
+        return courtFound[0];
+
 
 DB_TO_ADD = "db_production.db"
+NEW_URBA_NAME = "TEST URBA"
+NEW_INFO_URBA_MSG = 'Horarios de 9:00 a 22:00'
+NEW_COURT_NAME = "Padel Test"
+
 if __name__ == "__main__":
     conn = sqlite3.connect(DB_TO_ADD, check_same_thread=False)
     urbaGenerator = UrbaClientGenerator(conn)
-    # urba_id = urbaGenerator.create_urba('TEST URBA', 'Burgos - horarios de 9:00 a 22:00')
+    # urba_id = urbaGenerator.create_urba(NEW_URBA_NAME, NEW_INFO_URBA_MSG)
+    urba_id = urbaGenerator.get_urba_by_name(NEW_URBA_NAME)
     print(urba_id)
+    court_id = urbaGenerator.create_court(NEW_COURT_NAME, urba_id)
+    # court_id = urbaGenerator.get_court_by_name(NEW_COURT_NAME)
+    print(court_id)
